3e8cd9dff5b6a0f24c3e530d547fc06f533f0338
KimitaSora <kamaaya1118@gmail.com>
2019-04-26 14:51:03 +0900

    各メンバーのファイルを作成。シングルトンを作成。

---
+++ DOTPON/Assets/Scripts/SingletonMonoBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// シングルトン基底クラス
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour
+{
+    public static T instance;
+
+    protected virtual void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (instance == this)
+        {
+            instance = null;
+        }
+    }
+}
