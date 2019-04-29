bdf600cc7a5c4a5724aafa6226d0b16c250cd297
松田幸介 <darby8928@gmail.com>
2019-04-29 20:50:44 +0900

    視界(colider)に入ったらプレイヤーを追うようにした

---
+++ DOTPON/Assets/Member/Matsuda/Scripts/enemy_m.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_m : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    Vector3 vector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vector = transform.forward;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += vector * 0.03f;
+        Debug.Log(transform.forward);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(other == player.GetComponent<BoxCollider>())
+        {
+            transform.LookAt(player.transform.position);
+            vector = transform.forward;
+        }
+    }
+}
---
+++ DOTPON/Assets/Member/Matsuda/Scripts/plaer_m.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plaer_m : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        KeyInout();
+    }
+    void KeyInout()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            PlayerMove(new Vector3(0, 0, 5));
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            PlayerMove(new Vector3(-5, 0, 0));
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            PlayerMove(new Vector3(0, 0, -5));
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            PlayerMove(new Vector3(5, 0, 0));
+        }
+    }
+    void PlayerMove(Vector3 vec)
+    {
+        GetComponent<Rigidbody>().velocity += vec;
+    }
+}
