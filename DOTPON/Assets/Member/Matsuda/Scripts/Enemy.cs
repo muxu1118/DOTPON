e121b8434ebb33225aa0575428bdd61f539f612d
松田幸介 <darby8928@gmail.com>
2019-04-29 21:13:30 +0900

    enemyの基礎クラス作成。実装はまだ

---
+++ DOTPON/Assets/Member/Matsuda/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy
+{
+    int HP;
+    int DropDotNumber;
+
+    public void SpawnEnemy()
+    {
+        /*判定場所は後々
+        現在のモンスター数を取得。一定数以上だったらreturn
+        前回の生成からの時間を取得。
+        ５秒経っているか
+        */
+        //4か所にenemy生成
+        //enemyに対応した情報を持たせる
+    }
+
+    public void DropDot()
+    {
+        /*
+         * HP取得。0以外return
+        */
+        //ドロップするドット数取得
+        //マップにドロップを生成する処理
+        //enemyの消去
+    }
+}
