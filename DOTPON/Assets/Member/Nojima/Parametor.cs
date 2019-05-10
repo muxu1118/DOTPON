0474e91a9b1a0e0290ecf86c3be8d1f565792071
nojima <>
2019-05-10 10:24:21 +0900

    見やすくなるように修正

--- DOTPON/Assets/Member/Nojima/Parametor.cs
+++ DOTPON/Assets/Member/Nojima/Parametor.cs
@@ -2,37 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Parametor : MonoBehaviour
-{
-
     [CreateAssetMenu(menuName = "MyGame/Create ParameterTable", fileName = "ParameterTable")]
-    public class Status : ScriptableObject
+    public class Parametor : ScriptableObject
     {
         [SerializeField]
-        private int attackSpeed; //攻撃速度
-
+        [HeaderAttribute("攻撃速度")]
+        public int attackSpeed; //攻撃速度
+  
         [SerializeField]
+        [HeaderAttribute("攻撃力")]
         private int attackDamage; //威力
 
         [SerializeField]
+        [HeaderAttribute("必要ドット数")]
         private int necessaryDot; //必要ドット数
 
+
         [SerializeField]
+        [HeaderAttribute("耐久値")]
         private int durableValue; //耐久値
 
         [SerializeField]
+        [HeaderAttribute("武器のオブジェクト")]
         private GameObject weapon; //武器のオブジェクト
 
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+ 
