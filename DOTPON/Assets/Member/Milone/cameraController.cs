e933ca7977c730303d7e026c7b25579fcbb06d07
tokyotech-hosyu <tokyotech-hosyu@4900.co.jp>
2019-05-10 14:01:05 +0900

    knockbackのため,時間に合わせて色が変更する。

--- DOTPON/Assets/Member/Milone/cameraController.cs
+++ DOTPON/Assets/Member/Milone/cameraController.cs
@@ -6,17 +6,53 @@ public class cameraController : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset;
-
+     public float rotateSpeed=45f;
+    private float rotateInput;
+    private bool rotateAroundPlayer=false;
+    
+    
    
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        offset = transform.position - target.position;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = target.position - offset;
+        
+
+       /* if(Input.GetKey(KeyCode.Q)|| Input.GetKey(KeyCode.E))
+        {
+            
+            rotateAroundPlayer = true;
+            if (Input.GetKey(KeyCode.Q)){
+               rotateInput  = rotateSpeed;
+                
+            }
+            else
+            {
+                rotateInput = -rotateSpeed;
+            }
         }
+        else
+        {
+            rotateInput= 0f;
+            rotateAroundPlayer = false;
+        }
+       
+
+     if (rotateAroundPlayer)
+        {
+            Quaternion camrotation = Quaternion.AngleAxis(rotateInput* Time.deltaTime, Vector3.up);
+            offset = camrotation * offset;
+        }*/
+        
+        
+           // transform.LookAt(target);
+        
+        transform.position = target.position+offset;
+    }
 }
