38d45e84687c3d5968d4b74f18046ed01fe95c85
nojima <>
2019-05-17 11:37:48 +0900

    コントローラーのボタンの対応

--- DOTPON/Assets/Member/Nojima/ButtonControll.cs
+++ DOTPON/Assets/Member/Nojima/ButtonControll.cs
@@ -5,74 +5,104 @@ using UnityEngine;
 public class ButtonControll : MonoBehaviour
 {
     //string x;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    int PlayerNumber;//Playerの人数
+
+    //人数の分だけ動かす
+
     void Update()
     {
         Button();
+
+        ControllerNum();
+
+
+    }
+
+    void ControllerNum()
+    {
+        var controllerNames = Input.GetJoystickNames();
+
+        string[] cName = Input.GetJoystickNames();
+        PlayerNumber = 0;
+        for (int i = 0; i < cName.Length; i++)
+        {
+            if (cName[i] != "")
+            {
+                PlayerNumber++;
+            }
+        }
+        Debug.Log(PlayerNumber);//Player(接続されてるコントローラー)の数
     }
     private void Button()
     {
-    //    if (Input.GetKeyDown(x))
-    //    {
-    //        switch ()
-    //    }
+        //    if (Input.GetKeyDown(x))
+        //    {
+        //        switch ()
+        //    }
 
-        if (Input.GetKeyDown("joystick 1 button 0"))
-        {
-            Debug.Log("1P1");
-        }
-        if (Input.GetKeyDown("joystick 1 button 1"))
-        {
-            Debug.Log("1P2");
-        }
-        if (Input.GetKeyDown("joystick 1 button 2"))
-        {
-            Debug.Log("1P3");
-        }
-        if (Input.GetKeyDown("joystick 1 button 3"))
-        {
-            Debug.Log("1P4");
-        }
-        if (Input.GetKeyDown("joystick 1 button 4"))
-        {
-            Debug.Log("1P5");
-        }
-        if (Input.GetKeyDown("joystick 1 button 5"))
-        {
-            Debug.Log("1P6");
-        }
+        //for(int PadNum = 1;PadNum <= PlayerNumber; PadNum++)
+        //{
+        //    if(Input.GetButtonDown("joystick" + PlayerNumber + "_1"))
+        //    {
+        //        Debug.Log("1P1");
+        //    }
+        //}
 
-        if (Input.GetKeyDown("joystick 2 button 0"))
-        {
-            Debug.Log("2P1");
-        }
-        if (Input.GetKeyDown("joystick 2 button 1"))
-        {
-            Debug.Log("2P2");
-        }
-        if (Input.GetKeyDown("joystick 2 button 2"))
-        {
-            Debug.Log("2P3");
-        }
-        if (Input.GetKeyDown("joystick 2 button 3"))
-        {
-            Debug.Log("2P4");
-        }
-        if (Input.GetKeyDown("joystick 2 button 4"))
-        {
-            Debug.Log("2P5");
-        }
-        if (Input.GetKeyDown("joystick 2 button 5"))
-        {
-            Debug.Log("2P6");
-        }
 
+
+
+
+
+        //if (Input.GetKeyDown("joystick 1 button 0"))
+        //{
+        //    Debug.Log("1P1");
+        //}
+        //if (Input.GetKeyDown("joystick 1 button 1"))
+        //{
+        //    Debug.Log("1P2");
+        //}
+        //if (Input.GetKeyDown("joystick 1 button 2"))
+        //{
+        //    Debug.Log("1P3");
+        //}
+        //if (Input.GetKeyDown("joystick 1 button 3"))
+        //{
+        //    Debug.Log("1P4");
+        //}
+        //if (Input.GetKeyDown("joystick 1 button 4"))
+        //{
+        //    Debug.Log("1P5");
+        //}
+        //if (Input.GetKeyDown("joystick 1 button 5"))
+        //{
+        //    Debug.Log("1P6");
+        //}
+
+        //if (Input.GetKeyDown("joystick 2 button 0"))
+        //{
+        //    Debug.Log("2P1");
+        //}
+        //if (Input.GetKeyDown("joystick 2 button 1"))
+        //{
+        //    Debug.Log("2P2");
+        //}
+        //if (Input.GetKeyDown("joystick 2 button 2"))
+        //{
+        //    Debug.Log("2P3");
+        //}
+        //if (Input.GetKeyDown("joystick 2 button 3"))
+        //{
+        //    Debug.Log("2P4");
+        //}
+        //if (Input.GetKeyDown("joystick 2 button 4"))
+        //{
+        //    Debug.Log("2P5");
+        //}
+        //if (Input.GetKeyDown("joystick 2 button 5"))
+        //{
+        //    Debug.Log("2P6");
+        //}
 
     }
 }
