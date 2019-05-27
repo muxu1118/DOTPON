a5a8bc70a80c2d81043111138f99fbd7c0a68c9d
nojima <>
2019-05-27 15:40:55 +0900

    コントローラーで移動をできるように

---
+++ DOTPON/Assets/Member/Nojima/MoveController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    [SerializeField]
+    float run; //走る速度
+    [SerializeField]
+    float walk; //歩く速度
+
+    GameObject Player;
+    void Update()
+    {
+        MoveInput();
+    }
+    void Start()
+    {
+        Player = GameObject.Find("Cube");
+    }
+    void MoveInput()
+    {
+        if (Input.GetAxis("Vertical1") > 0.7)
+        {   //走る
+            Player.transform.position += transform.forward * run * Time.deltaTime;
+        }
+        if (Input.GetAxis("Vertical1") > 0.3){
+            //歩き
+            Player.transform.position += transform.forward * walk * Time.deltaTime;
+        }
+        if(Input.GetAxis("Vertical1") < -0.3)
+        {
+            //後ろに進む
+            Player.transform.position -= transform.forward * walk * Time.deltaTime;
+        }
+        if(Input.GetAxis("Horizontal1") > 0.4)
+        {
+            //右に進む
+            Player.transform.position += transform.right * walk * Time.deltaTime;
+        }
+        if (Input.GetAxis("Horizontal1") < -0.4)
+        {
+            //左に進む
+            Player.transform.position -= transform.right * walk * Time.deltaTime;
+        }
+    }
+}
