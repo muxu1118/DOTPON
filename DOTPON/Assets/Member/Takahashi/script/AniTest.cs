04c9de9121757fc1817268875a9de4689806b7ed
natuto1998 <kouta.taka.0811.15@gmail.com>
2019-05-27 16:56:59 +0900

    アニメーションの追加

---
+++ DOTPON/Assets/Member/Takahashi/script/AniTest.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AniTest : MonoBehaviour
+{
+
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetTrigger("Attack");
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            anim.SetTrigger("Attack2");
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetTrigger("Create");
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            anim.SetTrigger("Damage");
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            anim.SetTrigger("Run");
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            anim.SetTrigger("Walk");
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            anim.SetTrigger("modoru");
+        }
+    }
+}
