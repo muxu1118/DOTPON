923e41c195cfcabae9ddc88feb5be3e60f7c8bae
松田幸介 <darby8928@gmail.com>
2019-06-06 22:35:09 +0900

    8カ所からのランダムに4体スポーン

--- DOTPON/Assets/Member/Matsuda/SpownController.cs
+++ DOTPON/Assets/Member/Matsuda/SpownController.cs
@@ -8,41 +8,58 @@ public class SpownController : MonoBehaviour
     [SerializeField] Vector3[] positions;
     float time;
     int num;
+
+    [SerializeField] float spownDelay;
     // Start is called before the first frame update
     void Start()
     {
-        CreateEnemy();
-        //_object.transform.position = positions[Random.Range(0, 4)];
-        //for (int i = 0; i < 2; i++)
-        //{
-        //    for (int j = 0; j < 2; j++)
-        //    {
-        //        GameObject chald = Instantiate(obj[0], new Vector3(obj[0].transform.localPosition.x + i, 1, obj[0].transform.localPosition.z + j), Quaternion.identity);
-        //        chald.name = chald.name + (i + j);
-        //        chald.transform.parent = _object.transform;
-
-        //    }
-        //}
-        //_object.AddComponent<GoburinFlock>();
+        List<int> createdPos = new List<int>() {-1 };
+        for (int i = 0; i < 4; i++)
+        {
+            createdPos.Add(CreateEnemy(createdPos));
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         time += Time.deltaTime;
-        if(time / 10 >= 1)
+        if(time /  spownDelay>= 1)
         {
-            CreateEnemy();
+            List<int> createdPos = new List<int>() {-1};
+            for (int i = 0;i < 4;i++)
+            {
+                createdPos.Add(CreateEnemy(createdPos));
+            }
+            time = 0;
         }
     }
-    void CreateEnemy()
+    int CreateEnemy(List<int> createdPos)
     {
-        switch (2)
+        bool isCreated = true;
+        int posNum = 0;
+        while (isCreated)
+        {
+            posNum = Random.Range(0,positions.Length);
+            if (createdPos[0] == -1)
+            {
+                createdPos.RemoveAt(0);
+                break;
+            }
+            for (int i = 0;i < createdPos.Count; i++)
+            {
+                if (posNum != createdPos[i])
+                {
+                    isCreated = false;
+                }
+            }
+        }
+        switch (Random.Range(0,3))
         {
             case 0:
                 GameObject parentObject = new GameObject("GoburinFlock");
                 parentObject.tag = "enemy";
-                Vector3 spownPos = parentObject.transform.position = positions[Random.Range(0, 4)];
+                Vector3 spownPos = parentObject.transform.position = positions[posNum];
                 for (int i = 0; i < 2; i++)
                 {
                     for (int j = 0; j < 2; j++)
@@ -56,17 +73,17 @@ public class SpownController : MonoBehaviour
                 parentObject.AddComponent<GoburinFlock>();
                 break;
             case 1:
-                GameObject slime = Instantiate(obj[1], positions[Random.Range(0, 4)], Quaternion.identity);
+                GameObject slime = Instantiate(obj[1], positions[posNum], Quaternion.identity);
                 slime.name = slime.name + num;
                 break;
             case 2:
-                GameObject golem = Instantiate(obj[2], positions[Random.Range(0, 4)], Quaternion.identity);
+                GameObject golem = Instantiate(obj[2], positions[posNum], Quaternion.identity);
                 golem.name = golem.name + num;
                 break;
             default:
                 break;
         }
         num++;
-        time = 0;
+        return posNum;
     }
 }
