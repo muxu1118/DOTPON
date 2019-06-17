2bf798752dac6632bb60dc2800cff6a89cc55985
KimitaSora <kamaaya1118@gmail.com>
2019-06-17 18:51:42 +0900

    Input関連のテスト

--- DOTPON/Assets/Member/Kimita/MultiPlayerTestFolder/GamePadInput.cs
+++ DOTPON/Assets/Member/Kimita/MultiPlayerTestFolder/GamePadInput.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class GamePadInput : MonoBehaviour
 {
     InputManager IM;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        float h1 = Input.GetAxis("Pad1L_H");
+        float v1 = Input.GetAxis("Pad1L_V");
+        Debug.Log("1" + h1 + "," + v1);
+        float h2 = Input.GetAxis("Pad2L_H");
+        float v2 = Input.GetAxis("Pad2L_V");
+        Debug.Log("2" + h2 + "," + v2);
     }
 }
