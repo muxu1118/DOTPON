07dc223d7449208992c3e1227fb5247d29bd2500
takahashi <kouta.taka.0811.15@gmail.com>
2019-06-25 18:59:45 +0900

    爆弾、盾の処理(未完成)と盾と爆弾のアニメーション再生用の箱を追加

---
+++ DOTPON/Assets/Member/Takahashi/script/Bomb.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{        
+    public Parametor parametor;
+    AudioManager audioManager; 
+    
+    void Start()
+    {
+        audioManager = GameObject.Find("Manager").GetComponent<AudioManager>();
+        StartCoroutine("bom");
+    }
+
+    IEnumerator bom()
+    {
+        yield return new WaitForSeconds(3.0f);
+
+        //エフェクトを再生させる
+        //audioManager.playSfx(3);
+
+        BombAttack();
+        yield return new WaitForSeconds(0.5f);
+        Destroy(gameObject);
+    }
+
+    /// <summary>
+    /// 爆弾の処理
+    /// </summary>
+    private void BombAttack()
+    {
+        Collider[] targets = Physics.OverlapSphere(transform.position, 0.7f);
+        foreach(Collider obj in targets)
+        {
+            if(obj.tag == "Player")
+            {
+                obj.gameObject.GetComponent<Player>().Damage(parametor.attackDamage);
+            }
+            else if(obj.tag == "Enemy")
+            {
+                obj.gameObject.GetComponent<Enemy>().Damage(parametor.attackDamage);
+            }
+        }
+    }
+}
---
+++ DOTPON/Assets/Member/Takahashi/script/Shield.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    public Parametor parametor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "weapon")
+        {
+            //transform.root.gameObject.GetComponent<BoxCollider>().enabled = false;
+            StartCoroutine("shild");
+
+        }
+    }
+    
+    IEnumerable shild()
+    {
+        yield return new WaitForSeconds(0.1f);
+        transform.root.gameObject.GetComponent<BoxCollider>().enabled = false;
+        yield return new WaitForSeconds(0.2f);
+        transform.root.gameObject.GetComponent<BoxCollider>().enabled = true;
+    }    
+}
