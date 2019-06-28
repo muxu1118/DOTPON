c251dc95311f8ee4df41b3a5b31f15686adf4563
apa01 <32323780+apa01@users.noreply.github.com>
2019-06-28 09:23:24 +0900

    MultiPlayerManager
    Ranking function

--- DOTPON/Assets/Member/Arga/PlayTest.cs
+++ DOTPON/Assets/Member/Arga/PlayTest.cs
@@ -10,7 +10,7 @@ public class PlayTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        MultiPlayerManager.instance.P1Dot = 1;
+        // MultiPlayerManager.instance.P1Dot = 1;
     }
 
     // Update is called once per frame
@@ -19,12 +19,12 @@ public class PlayTest : MonoBehaviour
         //  number.text = MultiPlayerManager.instance.TotalPlayer.ToString();
     }
 
-    public void Plus()
+    public void PlusPl()
     {
         MultiPlayerManager.instance.totalPlayer++;
     }
 
-    public void Minus()
+    public void MinusPl()
     {
         MultiPlayerManager.instance.totalPlayer--;
     }
@@ -57,4 +57,45 @@ public class PlayTest : MonoBehaviour
     {
         SceneManager.LoadScene(0);
     }
+
+    public void P1PlusDot()
+    {
+        MultiPlayerManager.instance.P1Dot++;
+    }
+    public void P2PlusDot()
+    {
+        MultiPlayerManager.instance.P2Dot++;
+    }
+
+    public void P3PlusDot()
+    {
+        MultiPlayerManager.instance.P3Dot++;
+    }
+
+    public void P4PlusDot()
+    {
+        MultiPlayerManager.instance.P4Dot++;
+    }
+
+    public void P1PlusStar()
+    {
+        MultiPlayerManager.instance.P1Star++;
+    }
+
+    public void P2PlusStar()
+    {
+        MultiPlayerManager.instance.P2Star++;
+    }
+
+    public void P3PlusStar()
+    {
+        MultiPlayerManager.instance.P3Star++;
+    }
+
+    public void P4PlusStar()
+    {
+        MultiPlayerManager.instance.P4Star++;
+    }
+
+
 }
---
+++ DOTPON/Assets/Member/Arga/testScript/test1.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class test1 : MonoBehaviour
+{
+    enum DotText
+    {
+        P1Text,
+        P2Text,
+        P3Text,
+        P4Text
+    }
+    [SerializeField]
+    DotText dotText;
+
+    private Text Dot_Text;
+    void Start()
+    {
+        Dot_Text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetDotText();
+    }
+    void SetDotText()
+    {
+        switch (dotText)
+        {
+            case DotText.P1Text:
+                Dot_Text.text = "×" + MultiPlayerManager.instance.P1Dot.ToString();
+                break;
+            case DotText.P2Text:
+                Dot_Text.text = "×" + MultiPlayerManager.instance.P2Dot.ToString();
+                break;
+            case DotText.P3Text:
+                Dot_Text.text = "×" + MultiPlayerManager.instance.P3Dot.ToString();
+                break;
+            case DotText.P4Text:
+                Dot_Text.text = "×" + MultiPlayerManager.instance.P4Dot.ToString();
+                break;
+        }
+    }
+}
