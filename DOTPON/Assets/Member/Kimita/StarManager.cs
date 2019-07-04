63adcafff59d4749142195a0afa7b614d4f6bc36
KimitaSora <kamaaya1118@gmail.com>
2019-07-04 11:30:18 +0900

    プレイヤーが死んだら☆を生成。減らすのはマネージャーでやってます。

--- DOTPON/Assets/Member/Kimita/StarManager.cs
+++ DOTPON/Assets/Member/Kimita/StarManager.cs
@@ -35,6 +35,36 @@ public class StarManager : SingletonMonoBehaviour<StarManager>
 
 
     /// <summary>
+    /// スターの出現 (出現される場所と減らすプレイヤー)
+    /// </summary>
+    public void DeadStarDrop( Vector3 vec3,Player.PlayerKind PK)
+    {
+        switch (PK)
+        {
+            case Player.PlayerKind.Player1:
+                MultiPlayerManager.instance.P1Star--;
+                break;
+            case Player.PlayerKind.Player2:
+                MultiPlayerManager.instance.P2Star--;
+                break;
+            case Player.PlayerKind.Player3:
+                MultiPlayerManager.instance.P3Star--;
+                break;
+            case Player.PlayerKind.Player4:
+                MultiPlayerManager.instance.P4Star--;
+                break;
+            default:
+                Debug.LogError("よばれちゃいけんのやぞ");
+                break;
+        }
+        // dotObjのスクリプトを読み込み出現に時間を加える
+        // DotScript Dot = dotObj.GetCompoment<DotScript>();
+        // Dot.Time = time;
+        // ドットの出現(位置はランダム)
+        Instantiate(starObject, new Vector3(vec3.x, 1, vec3.z), Quaternion.identity).name = "Star";
+    }
+
+    /// <summary>
     /// スターを作成する関数
     /// </summary>
     public void StarCreate(/*MultiPrayerかプレイヤーのスクリプト*/)
