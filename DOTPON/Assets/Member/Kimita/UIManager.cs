d16bc39077fee9e7b966bffcd79a4687b276fc8d
KImitaSora <kamaaya1118@gmail.com>
2019-07-08 18:51:43 +0900

    Uiの位置調整Canvasの位置を保存する変数の作成

--- DOTPON/Assets/Member/Kimita/UIManager.cs
+++ DOTPON/Assets/Member/Kimita/UIManager.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class UIManager : SingletonMonoBehaviour<UIManager>
 {
+    int playerNumber; // プレイヤーの数を補完
+
+    // Canvasの位置
+    List<Vector2> canvasPosis;
+
     // Start is called before the first frame update
     void Start()
     {
+        playerNumber = MultiPlayerManager.instance.totalPlayer;
         
     }
 
