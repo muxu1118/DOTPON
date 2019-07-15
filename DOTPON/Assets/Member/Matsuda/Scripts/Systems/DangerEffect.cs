c2824b0d9cc715ec2b2f67b2b846d77c2b211b4b
matsuda <darby8928@gmail.com>
2019-07-15 16:26:43 +0900

    ハンマーとスターのモデル

---
+++ DOTPON/Assets/Member/Matsuda/Scripts/Systems/DangerEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DangerEffect : MonoBehaviour
+{
+    Image img;
+    // Start is called before the first frame update
+    void Start()
+    {
+        img = GetComponent<Image>();
+        img.color = Color.clear;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.img.color = new Color(0.5f,0,0,0.5f);
+        }
+        else
+        {
+            this.img.color = Color.Lerp(this.img.color,Color.clear,Time.deltaTime);
+        }
+    }
+}
