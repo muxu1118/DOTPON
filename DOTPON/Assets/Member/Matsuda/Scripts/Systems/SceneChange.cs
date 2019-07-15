e12c2641e18553a62871646fada300c348911ec5
matsuda <darby8928@gmail.com>
2019-07-15 19:12:15 +0900

    画面切り替えのドット降らせる奴作ってる途中

--- DOTPON/Assets/Member/Matsuda/Scripts/Systems/SceneChange.cs
+++ DOTPON/Assets/Member/Matsuda/Scripts/Systems/SceneChange.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SceneChange : MonoBehaviour
 {
+    [SerializeField] GameObject img;
+    float x, y;
     // Start is called before the first frame update
     void Start()
     {
-        
+        x = Screen.width;
+        y = Screen.height;
+        SwitchScene();
+    }
+    
+
+    public void SwitchScene()
+    {
+        StartCoroutine(DropDot());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator DropDot()
     {
-        
+        GameObject game = new GameObject("SceneEffect");
+        game.transform.SetParent(GameObject.Find("Canvas").transform, false);
+        int count = 0;
+        while (count <= 180)
+        {
+            Instantiate(img, new Vector3(Random.Range(-x, x++), y, 0), Quaternion.identity).transform.SetParent(game.transform,false);
+            count++;
+            yield return null;
+        }
     }
 }
