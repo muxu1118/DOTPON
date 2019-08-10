e28d0ede624cdd37831c60d5726502ead2b5a022
matsuda <darby8928@gmail.com>
2019-08-11 01:40:20 +0900

    PV用5

--- DOTPON/Assets/Member/Matsuda/Scripts/Fiest.cs
+++ DOTPON/Assets/Member/Matsuda/Scripts/Fiest.cs
@@ -21,13 +21,53 @@ public class Fiest : MonoBehaviour
     }
     IEnumerator Move()
     {
-        for (int i = 0;i < 180;i++)
+        yield return new WaitForSeconds(0.5f);
+        light[1].SetActive(true);
+        yield return new WaitForSeconds(0.5f);
+        player[0].GetComponent<Animator>().SetTrigger("Walk");
+        for (int i = 0; i < 100; i++)
         {
+
             player[0].transform.localPosition += player[0].transform.forward * 3 * Time.deltaTime;
-            player[0].GetComponent<Animator>().SetFloat("Speed",0.4f);
+            //player[0].GetComponent<Animator>().SetFloat("Speed", 0.4f);
             yield return null;
         }
-        player[0].GetComponent<Animator>().SetFloat("Speed", 0f);
+        player[0].GetComponent<Animator>().SetTrigger("Walk_re");
+        //player[0].GetComponent<Animator>().SetFloat("Speed", 0f);
+        //player[0].GetComponent<Animator>().SetTrigger("PV1");
+        //yield return new WaitForSeconds(1.5f);
+        //player[0].GetComponent<Animator>().SetTrigger("PV1_re");
+        //for (int i = 0; i < 30; i++)
+        //{
+        //    player[0].transform.localPosition += player[0].transform.forward * 3 * Time.deltaTime;
+        //    player[0].GetComponent<Animator>().SetFloat("Speed", 0.4f);
+        //    yield return null;
+        //}
+        //player[0].GetComponent<Animator>().SetFloat("Speed", 0f);
+        //player[0].GetComponent<Animator>().SetTrigger("PV2");
+        //yield return new WaitForSeconds(1.5f);
+        //player[0].GetComponent<Animator>().SetTrigger("PV2_re");
+        //for (int i = 0; i < 30; i++)
+        //{
+        //    player[0].transform.localPosition += player[0].transform.forward * 3 * Time.deltaTime;
+        //    player[0].GetComponent<Animator>().SetFloat("Speed", 0.4f);
+        //    yield return null;
+        //}
+        //player[0].GetComponent<Animator>().SetFloat("Speed", 0f);
+        //player[0].GetComponent<Animator>().SetTrigger("PV3");
+        //yield return new WaitForSeconds(1.5f);
+        //player[0].GetComponent<Animator>().SetTrigger("PV3_re");
+        //for (int i = 0; i < 30; i++)
+        //{
+        //    player[0].transform.localPosition += player[0].transform.forward * 3 * Time.deltaTime;
+        //    player[0].GetComponent<Animator>().SetFloat("Speed", 0.4f);
+        //    yield return null;
+        //}
+
+        //player[0].GetComponent<Animator>().SetFloat("Speed", 0f);
+        light[0].SetActive(true);
+        light[1].SetActive(false);
+        RenderSettings.skybox = mat;
         yield return new WaitForSeconds(1);
         for (int i = 0; i < 60; i++)
         {
@@ -37,24 +77,25 @@ public class Fiest : MonoBehaviour
             //                                                     1 * Time.deltaTime);
             yield return null;
         }
-        light[0].SetActive(true);
-        light[1].SetActive(false);
-        RenderSettings.skybox = mat;
         yield return new WaitForSeconds(1);
-        for (int i = 0; i < 180; i++)
+        for (int i = 0; i < 240; i++)
         {
             for (int j = 0;j < 4;j++)
             {
-                player[j].transform.localPosition += player[j].transform.forward * 4 * Time.deltaTime;
-                player[j].GetComponent<Animator>().SetFloat("Speed", 0.8f);
+
+                if (j == 0)
+                {
+                    player[j].transform.localPosition += player[j].transform.forward * 2 * Time.deltaTime;
+                    player[0].GetComponent<Animator>().SetTrigger("jump");
+                }
             }
             yield return null;
-            if (i == 60)
+            if (i == 30)
             {
-                player[0].GetComponent<Animator>().SetTrigger("jump");
-                player[1].GetComponent<Animator>().SetTrigger("PunchAttack");
-                player[2].GetComponent<Animator>().SetTrigger("PunchAttack");
-                player[3].GetComponent<Animator>().SetTrigger("PunchAttack");
+                
+                player[1].GetComponent<Animator>().SetTrigger("PV1");
+                player[2].GetComponent<Animator>().SetTrigger("PV2");
+                player[3].GetComponent<Animator>().SetTrigger("PV3");
             }
         }
     }
