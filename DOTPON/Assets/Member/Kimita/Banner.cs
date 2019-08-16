5c2986c2eabe59bb4872071f267614876c158be2
kimitasora <kamaaya1118@gmail.com>
2019-08-16 17:50:20 +0900

    Banner.csの作成。リザルトアニメーションの調整

---
+++ DOTPON/Assets/Member/Kimita/Banner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Banner : MonoBehaviour
+{
+    public enum BannerNumber
+    {
+        firstPlayer,
+        secondPlayer,
+        thirdPlayer,
+        fourth
+    }
+    public BannerNumber ThisBanNum;
+    int dotCount;
+    int starCount;
+    Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //for(int i = 0;i<=<MultiPlayerManager.instance.)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
