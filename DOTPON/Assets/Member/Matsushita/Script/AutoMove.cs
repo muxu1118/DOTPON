63dc861d889b8e0f6bb719d770c9db17c83c4d60
matsushita <matsuniisan@gmail.com>
2019-08-23 11:28:19 +0900

    雲が流れるように設定しました。

---
+++ DOTPON/Assets/Member/Matsushita/Script/AutoMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoMove : MonoBehaviour
+{
+    [SerializeField]
+    float Speed = 1.0f;
+
+    float px = 0;
+    float pz = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        px = Random.Range(-5.0f, 5.0f);
+        pz = Random.Range(-5.0f, 5.0f);
+
+        transform.position = new Vector3(px, 10, pz);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * Speed * Time.deltaTime;
+
+        if (transform.position.z >= 25.0f)
+        {
+            transform.position = new Vector3(px, 10, pz);
+        }
+    }
+}
